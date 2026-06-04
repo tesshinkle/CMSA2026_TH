f8635e8619eaedba292bafde658fd6cc1118866f
tesshinkle <isabellajudehinkle@gmail.com>
2026-06-04 13:46:50 -0400

    EDA womens tennis project.

---
+++ EDA_project.R
@@ -0,0 +1,16 @@
+#CMSA EDA WTA Project
+
+library(tidyverse)
+
+wta_2021_2026_matches <- read_csv("https://raw.githubusercontent.com/36-SURE/2026/main/data/wta_matches_2021_2026.csv")
+
+wta_2021_2026_matches <-
+  map_dfr(c(2021:2026),
+          function(year) {
+            read_csv(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_",
+                            year, ".csv")) %>%
+              mutate(winner_seed = as.character(winner_seed),
+                     loser_seed = as.character(loser_seed))
+          })
+
+view(wta_2021_2026_matches)
